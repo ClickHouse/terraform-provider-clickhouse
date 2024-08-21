1abb9f59c9d3dc2452b66f49c6045be0765f100c
Christian Bianchi <whites11@gmail.com>
2024-08-21 14:23:45 +0200

    Refactor examples structure. Run private link test as part of e2e

--- examples/full/basic/gcp/provider.tf
+++ examples/full/basic/gcp/provider.tf
@@ -8,7 +8,7 @@ terraform {
   }
 }
 
-provider clickhouse {
+provider "clickhouse" {
   organization_id = var.organization_id
   token_key       = var.token_key
   token_secret    = var.token_secret
