a0185941398fdad9e519724f7c5589d92aa16d76
Christian Bianchi <whites11@gmail.com>
2024-11-15 15:31:10 +0100

    Fix attachment of multiple private endpoints to the same service

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ func main() {
 
 	flag.BoolVar(&debug, "debug", false, "set to true to run the provider with support for debuggers like delve")
 	flag.Parse()
-	providerserver.Serve(context.Background(), provider.New, providerserver.ServeOpts{
+	providerserver.Serve(context.Background(), provider.New, providerserver.ServeOpts{ //nolint:errcheck
 		Address: "clickhouse.cloud/terraform/clickhouse",
 		Debug:   debug,
 	})
