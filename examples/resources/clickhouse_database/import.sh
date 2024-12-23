99e8e9dcc7bd7f468cbc0dc493cafb57f87dbae8
Christian Bianchi <whites11@gmail.com>
2024-12-23 14:38:33 +0100

    add database examples

---
+++ examples/resources/clickhouse_database/import.sh
@@ -0,0 +1,2 @@
+# Database can be imported by specifying the service UUID and the database name, comma separated.
+terraform import clickhouse_database.example xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx,mydatabase
---
+++ examples/resources/clickhouse_database/resource.tf
@@ -0,0 +1,5 @@
+resource "clickhouse_database" "mydatabase" {
+  service_id = xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx
+  name = "mydatabase"
+  comment = "This is a test database"
+}
