ab57df346f70cf5d974b1c10b7eac13ac67d01ad
Kuba Kaflik <kuba.kaflik@clickhouse.com>
2025-04-08 15:11:01 +0200

    ClickPipe resource import support

---
+++ examples/resources/clickhouse_clickpipe/import.sh
@@ -0,0 +1,2 @@
+# ClickPipes can be imported by specifying both service ID and clickpipe ID.
+terraform import clickhouse_clickpipe.example xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx:xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx
