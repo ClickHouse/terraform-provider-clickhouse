bb1bf1eaca2cbd8b0dafd1cacde20714d8d44900
Christian Bianchi <whites11@gmail.com>
2025-04-17 15:42:20 +0200

    Improve plan management for the mysql endpoint attribute

--- pkg/resource/service.go
+++ pkg/resource/service.go
@@ -724,12 +724,10 @@ func (r *ServiceResource) ModifyPlan(ctx context.Context, req resource.ModifyPla
 
 			resp.Plan.Set(ctx, plan)
 		}
-
 	} else {
 		// User set a value to the mysql endpoint
 		// Nothing to do here as terraform manages this case correctly.
 	}
-
 }
 
 // Create a new resource
