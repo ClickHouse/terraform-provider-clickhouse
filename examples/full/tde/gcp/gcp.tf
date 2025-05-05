396b3de10223192c7ed8977e54941572e7d878c0
Christian Bianchi <whites11@gmail.com>
2025-05-05 18:07:13 +0200

    add gcp tde example with key rotation

--- examples/full/tde/gcp/gcp.tf
+++ examples/full/tde/gcp/gcp.tf
@@ -8,13 +8,13 @@ provider "google" {
 }
 
 resource "google_kms_key_ring" "key_ring" {
-  name     = replace(var.service_name, "/[^-a-zA-Z0-9_]/", "_")
+  name     = clickhouse_service.service.id
   project  = var.project_id
   location = var.region
 }
 
 resource "google_kms_crypto_key" "key" {
-  name                       = replace(var.service_name, "/[^-a-zA-Z0-9_]/", "_")
+  name                       = clickhouse_service.service.id
   key_ring                   = google_kms_key_ring.key_ring.id
   purpose                    = "ENCRYPT_DECRYPT"
   destroy_scheduled_duration = "86400s"
@@ -28,4 +28,30 @@ resource "google_kms_crypto_key" "key" {
     e2e = true,
     service = lower(replace(var.service_name, "/[^-a-zA-Z0-9_]/", "_"))
   }
+}
+
+data "google_client_openid_userinfo" "current" {}
+
+resource "google_kms_crypto_key_iam_binding" "owners" {
+  role          = "roles/owner"
+  crypto_key_id = google_kms_crypto_key.key.id
+  members       = ["user:${data.google_client_openid_userinfo.current.email}"]
+}
+
+resource "google_kms_crypto_key_iam_binding" "viewers" {
+  role          = "roles/cloudkms.viewer"
+  crypto_key_id = google_kms_crypto_key.key.id
+  members       = ["serviceAccount:${clickhouse_service.service.transparent_data_encryption.role_id}"]
+}
+
+resource "google_kms_crypto_key_iam_binding" "decrypters" {
+  role          = "roles/cloudkms.cryptoKeyDecrypter"
+  crypto_key_id = google_kms_crypto_key.key.id
+  members       = ["serviceAccount:${clickhouse_service.service.transparent_data_encryption.role_id}"]
+}
+
+resource "google_kms_crypto_key_iam_binding" "encrypters" {
+  role          = "roles/cloudkms.cryptoKeyEncrypter"
+  crypto_key_id = google_kms_crypto_key.key.id
+  members       = ["serviceAccount:${clickhouse_service.service.transparent_data_encryption.role_id}"]
 }
