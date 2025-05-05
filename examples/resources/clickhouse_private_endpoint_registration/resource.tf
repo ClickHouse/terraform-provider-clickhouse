51e980103822a19ede4ad60c14adae847738a8f6
Christian Bianchi <whites11@gmail.com>
2025-05-05 09:36:36 +0200

    private endpoint refactoring (#286)

--- examples/resources/clickhouse_private_endpoint_registration/resource.tf
+++
@@ -1,6 +0,0 @@
-resource "clickhouse_private_endpoint_registration" "endpoint" {
-  cloud_provider      = "aws"
-  private_endpoint_id = "vpce-..."
-  region              = "us-west-2"
-  description         = "Private Link from VPC foo"
-}
