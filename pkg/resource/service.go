3cf924c4341e8e571fa0fe8338f2be4112ab55bd
Christian Bianchi <whites11@gmail.com>
2025-06-04 13:48:49 +0200

    Fix validation of password fields for secondary services (#307)

--- pkg/resource/service.go
+++ pkg/resource/service.go
@@ -95,6 +95,12 @@ func (r *ServiceResource) Schema(_ context.Context, _ resource.SchemaRequest, re
 					stringplanmodifier.UseStateForUnknown(),
 					stringplanmodifier.RequiresReplace(),
 				},
+				Validators: []validator.String{
+					stringvalidator.ConflictsWith(path.Expressions{
+						path.MatchRoot("password"),
+						path.MatchRoot("password_hash"),
+					}...),
+				},
 			},
 			"readonly": schema.BoolAttribute{
 				Description: "Indicates if this service should be read only. Only allowed for secondary services, those which share data with another service (i.e. when `warehouse_id` field is set).",
@@ -140,7 +146,7 @@ func (r *ServiceResource) Schema(_ context.Context, _ resource.SchemaRequest, re
 						regexp.MustCompile(`^(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)?$`),
 						"must be a base64 encoded hash",
 					),
-					stringvalidator.ConflictsWith(path.Expressions{path.MatchRoot("password"), path.MatchRoot("warehouse_id")}...),
+					stringvalidator.ConflictsWith(path.Expressions{path.MatchRoot("password")}...),
 				},
 			},
 			"double_sha1_password_hash": schema.StringAttribute{
