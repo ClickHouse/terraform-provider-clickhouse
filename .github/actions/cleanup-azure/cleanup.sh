faea3be562c665548dc238b827328c1e6fdafcf1
Gabriel Martinez <gabriel.martinezdeoliveira@clickhouse.com>
2025-09-09 16:45:32 +0100

    validate resource group

--- .github/actions/cleanup-azure/cleanup.sh
+++ .github/actions/cleanup-azure/cleanup.sh
@@ -6,5 +6,11 @@ SERVICE_NAME="${SERVICE_NAME:?"SERVICE_NAME cannot be empty"}"
 
 ### Resource Group
 echo "::group::Deleting Resource Group..."
-az group delete --name "${SERVICE_NAME}" --yes
+
+if az group show --name "${SERVICE_NAME}" >/dev/null 2>&1; then
+  az group delete --name "${SERVICE_NAME}" --yes
+else
+  echo "Resource group '${SERVICE_NAME}' does not exist, skipping deletion."
+fi
+
 echo "::endgroup::"
