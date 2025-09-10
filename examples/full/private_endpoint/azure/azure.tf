574bdbe743c927ad5b3ba2e1364a9966bf76ca38
Gabriel Martinez <gabriel.martinezdeoliveira@clickhouse.com>
2025-09-10 11:32:44 +0100

    use actions for common steps

--- examples/full/private_endpoint/azure/azure.tf
+++ examples/full/private_endpoint/azure/azure.tf
@@ -14,21 +14,19 @@ provider "azurerm" {
 }
 
 locals {
-  service_name_parsed = replace(replace(var.service_name, "[", "("), "]", ")")
-
   tags = {
-    Name = local.service_name_parsed
+    Name = var.service_name
   }
 }
 
 resource "azurerm_resource_group" "this" {
-  name     = local.service_name_parsed
+  name     = var.service_name
   location = var.location
   tags     = local.tags
 }
 
 resource "azurerm_virtual_network" "this" {
-  name                = local.service_name_parsed
+  name                = var.service_name
   address_space       = ["10.0.0.0/16"]
   location            = var.location
   resource_group_name = azurerm_resource_group.this.name
@@ -36,30 +34,30 @@ resource "azurerm_virtual_network" "this" {
 }
 
 resource "azurerm_subnet" "this" {
-  name                 = local.service_name_parsed
+  name                 = var.service_name
   resource_group_name  = azurerm_resource_group.this.name
   virtual_network_name = azurerm_virtual_network.this.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
 resource "azurerm_private_endpoint" "this" {
-  name                = local.service_name_parsed
+  name                = var.service_name
   location            = var.location
   resource_group_name = azurerm_resource_group.this.name
   subnet_id           = azurerm_subnet.this.id
 
   private_service_connection {
-    name                              = local.service_name_parsed
+    name                              = var.service_name
     private_connection_resource_alias = clickhouse_service.this.private_endpoint_config.endpoint_service_id
     is_manual_connection              = true
-    request_message                   = "clickhouse-${local.service_name_parsed}"
+    request_message                   = "clickhouse-${var.service_name}"
   }
 
   tags = local.tags
 }
 
 resource "azurerm_network_security_group" "this" {
-  name                = local.service_name_parsed
+  name                = var.service_name
   location            = var.location
   resource_group_name = azurerm_resource_group.this.name
   tags                = local.tags
@@ -71,8 +69,8 @@ resource "azurerm_subnet_network_security_group_association" "this" {
 }
 
 resource "azurerm_network_security_rule" "this" {
-  name              = local.service_name_parsed
-  description       = "Allow subnet to ${local.service_name_parsed}"
+  name              = var.service_name
+  description       = "Allow subnet to ${var.service_name}"
   priority          = 100
   direction         = "Inbound"
   access            = "Allow"
