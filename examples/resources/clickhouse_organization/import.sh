cbc2114c5ba6425013bb880383022a65579199ef
Nuno Adrego <nuno.adrego@clickhouse.com>
2026-02-06 13:30:05 +0000

    feat(resource): shellcheck

--- examples/resources/clickhouse_organization/import.sh
+++ examples/resources/clickhouse_organization/import.sh
@@ -1,3 +1,3 @@
+#!/bin/bash
 # The organization resource can be imported using the organization ID
-# The organization ID is automatically determined from the provider configuration
 terraform import clickhouse_organization.org_settings xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx
