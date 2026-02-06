f328fc0a4d8393e1e3156dca9a39bd083968c663
Nuno Adrego <nuno.adrego@clickhouse.com>
2026-02-06 13:38:49 +0000

    feat(resource): add full organization example

---
+++ examples/full/organization/aws/main.tf
@@ -0,0 +1,17 @@
+variable "organization_id" {
+  type = string
+}
+
+variable "token_key" {
+  type      = string
+  sensitive = true
+}
+
+variable "token_secret" {
+  type      = string
+  sensitive = true
+}
+
+resource "clickhouse_organization" "this" {
+  core_dumps_enabled = true 
+}
