40f827a77d60b4a0c671e8e2e9c3f72dfd47fea3
Nuno Adrego <nuno.adrego@clickhouse.com>
2026-02-10 15:25:23 +0000

    feat(resource): move functions to new file

---
+++ pkg/internal/api/organization.go
@@ -0,0 +1,55 @@
+package api
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+	"strings"
+)
+
+// GetOrganization retrieves the current organization settings.
+func (c *ClientImpl) GetOrganization(ctx context.Context) (*OrgResult, error) {
+	req, err := http.NewRequest(http.MethodGet, c.getOrgPath(""), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	body, err := c.doRequest(ctx, req)
+	if err != nil {
+		return nil, err
+	}
+
+	orgResponse := ResponseWithResult[OrgResult]{}
+	err = json.Unmarshal(body, &orgResponse)
+	if err != nil {
+		return nil, err
+	}
+
+	return &orgResponse.Result, nil
+}
+
+// UpdateOrganization updates the organization settings.
+func (c *ClientImpl) UpdateOrganization(ctx context.Context, orgUpdate OrganizationUpdate) (*OrgResult, error) {
+	rb, err := json.Marshal(orgUpdate)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest(http.MethodPatch, c.getOrgPath(""), strings.NewReader(string(rb)))
+	if err != nil {
+		return nil, err
+	}
+
+	body, err := c.doRequest(ctx, req)
+	if err != nil {
+		return nil, err
+	}
+
+	orgResponse := ResponseWithResult[OrgResult]{}
+	err = json.Unmarshal(body, &orgResponse)
+	if err != nil {
+		return nil, err
+	}
+
+	return &orgResponse.Result, nil
+}
