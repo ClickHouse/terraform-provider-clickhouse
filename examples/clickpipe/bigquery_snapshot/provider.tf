1605ccef7665ee5dc8de8c7667c77c814b7e020c
Thomas Panetti <tpanetti95@gmail.com>
2026-04-10 08:03:25 -0700

    Revert "Bump provider version in examples to 3.14.0" (#504)

--- examples/clickpipe/bigquery_snapshot/provider.tf
+++ examples/clickpipe/bigquery_snapshot/provider.tf
@@ -2,7 +2,7 @@
 terraform {
   required_providers {
     clickhouse = {
-      version = "3.14.0"
+      version = "3.13.0-alpha1"
       source  = "ClickHouse/clickhouse"
     }
   }
--- examples/full/pci/aws/provider.tf
+++ examples/full/pci/aws/provider.tf
@@ -2,7 +2,7 @@
 terraform {
   required_providers {
     clickhouse = {
-      version = "3.14.0"
+      version = "3.12.0"
       source  = "ClickHouse/clickhouse"
     }
   }
