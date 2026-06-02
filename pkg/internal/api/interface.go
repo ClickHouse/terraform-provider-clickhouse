80d96caaf31c69c9a033066f59956d3a6f5b8a2c
Amog Iska <amogiska@gmail.com>
2026-06-02 09:31:24 -0700

    feat(api): add Managed Postgres HTTP client (#544)

--- pkg/internal/api/interface.go
+++ pkg/internal/api/interface.go
@@ -62,4 +62,18 @@ type Client interface {
 	CreateRole(ctx context.Context, req RoleCreateRequest) (*RBACRole, error)
 	UpdateRole(ctx context.Context, roleId string, req RoleUpdateRequest) (*RBACRole, error)
 	DeleteRole(ctx context.Context, roleId string) error
+
+	GetPostgres(ctx context.Context, postgresId string) (*Postgres, error)
+	ListPostgres(ctx context.Context) ([]PostgresListItem, error)
+	CreatePostgres(ctx context.Context, body PostgresCreate) (*Postgres, string, error)
+	UpdatePostgres(ctx context.Context, postgresId string, body PostgresUpdate) (*Postgres, error)
+	DeletePostgres(ctx context.Context, postgresId string) error
+	WaitForPostgresState(ctx context.Context, postgresId string, stateChecker func(string) bool, maxWaitSeconds int) error
+	WaitForPostgresStateTransitionAndReturn(ctx context.Context, postgresId string, terminalState string, maxWaitSeconds int) error
+	RestorePostgres(ctx context.Context, sourceId string, body PostgresRestoreRequest) (*Postgres, error)
+	SetPostgresPassword(ctx context.Context, postgresId string, body PostgresPassword) (*PostgresPassword, error)
+	CreatePostgresReadReplica(ctx context.Context, sourceId string, body PostgresReadReplicaRequest) (*Postgres, error)
+	GetPostgresConfig(ctx context.Context, postgresId string) (*PostgresConfig, error)
+	ReplacePostgresConfig(ctx context.Context, postgresId string, body PostgresConfig) (*PostgresConfigUpdateResponse, error)
+	GetPostgresCaCertificates(ctx context.Context, postgresId string) ([]byte, error)
 }
